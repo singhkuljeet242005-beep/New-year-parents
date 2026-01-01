077c71bf080c9907a69e62609a2de428e4401d54
singhkuljeet242005-beep <singhkuljeet242005@gmail.com>
2026-01-01 09:24:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Reveal on scroll
+const reveals = document.querySelectorAll('.reveal');
 
+function revealOnScroll() {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
